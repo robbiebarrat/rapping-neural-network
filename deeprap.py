7c4e7b55dcf0984948e192687d1b8bb1376ba224
Robbie Barrat <robbiebarrat@gmail.com>
2017-01-23 18:36:00 -0500

    complete redo
    updated the way trained networks are stored (now uses pybrain instead of pickle), made it rhyme better (the list of possible rhymes is saved with the trained network in a folder now), made code cleaner, etc.

--- deeprap.py
+++ deeprap.py
@@ -1,12 +1,22 @@
 # imports
 import re
-import pickle
+import os
 import random
 import pronouncing
-import pybrain
+import ast
 from pybrain.datasets import SupervisedDataSet
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.structure import TanhLayer
+from pybrain.tools.xml.networkwriter import NetworkWriter
+from pybrain.tools.xml.networkreader import NetworkReader
+
+#global variables (these affect the whole program)
+training = 0 #training of 0 means you just want to have it write a rap - training of 1 will train the network on the contents of lyrics.txt
+epoch = 200 #this is just the number of the folder you would like to load (folder contains the trained net and rhymes)
+
+if training == 1:
+    print "*Training Mode*"
 
 # counts syllables in word
 def syllablecount(word):
@@ -105,7 +115,7 @@ def writearap(start, net, rapdict, all_possible_rhymes, lyricsused, song):
     for i in range(0, 100):
         formatbar([rap[i][0], rap[i][1]], rapdict, all_possible_rhymes, lyricsused, song)
         formatbar([rap[i][2], rap[i][3]], rapdict, all_possible_rhymes, lyricsused, song)
-    if len(lyricsused) > 2:
+    if len(lyricsused) > 3: # this number can be adjusted - usually the short verses it generates are low quality.
         return lyricsused
     else:
         return []
@@ -118,7 +128,21 @@ def lyrics_used_in_song(song):
             used.append(line)
     return used
 
-# the main function -- i'll clean this up and divide it into smaller functions soon.
+def savenetwork(net, rhymelist, epoch):
+    os.mkdir(str(epoch))
+    r = open(str(epoch) + '/rhymelist', 'w')
+    r.write(str(rhymelist))
+    #n = open(str(epoch)) + '/network.xml', 'w'
+    NetworkWriter.writeToFile(net, str(epoch) + "/network.xml" )
+
+def opennetwork(epoch):
+    r = open(str(epoch) + '/rhymelist', 'r')
+    rhymelist = ast.literal_eval(str(r.read()))
+    net = NetworkReader.readFrom(str(epoch) + "/network.xml")
+    return net, rhymelist
+
+
+# the main function -- i'm in the process of cleaning this up and dividing it into smaller functions.
 def main():
     # the existing lyrics you've filled the text document with
     lyrics = open('lyrics.txt').read().split("\n")
@@ -127,61 +151,64 @@ def main():
     song = []
 
     # all of the possible rhymes based on the contents of the stuff you fed it
-    all_possible_rhymes = rhymeindex(lyrics)
+    if training == 1:
+        all_possible_rhymes = rhymeindex(lyrics)
+    elif training == 0:
+        all_possible_rhymes  = opennetwork(epoch)[1]
+        rhymes_in_lyrics = rhymeindex(lyrics)
+        for rhyme in rhymes_in_lyrics:
+            if rhyme not in all_possible_rhymes:
+                all_possible_rhymes.append(rhyme)
+        print all_possible_rhymes
+
+    if training == 1:
+        net = buildNetwork(4, 8, 8, 8, 8, 8, 8, 8, 8, 4, recurrent=True, hiddenclass=TanhLayer)
+        t = BackpropTrainer(net, learningrate=0.05, momentum=0.5, verbose=True)
+
+    # This loads a neural network that has already been trained on an actual rap song - so it knows how the rhymes and syllables should fit together
+    if training == 0:
+        rapdict = []
+        rhyme_list_generator(lyrics, rapdict, all_possible_rhymes)
+        net = opennetwork(epoch)[0]
+    t = BackpropTrainer(net, learningrate=0.01, momentum=0.5, verbose=True)
+
+
 
     # debug stuff...
     print "\n\nAlright, here are all of the possible rhymes from the lyrics it can draw from."
     print all_possible_rhymes
 
-    # rapdict is just the list containing smaller lists as follows;
-    # [the text of the line, the number of syllables in the line, the number of the rhyme scheme of the line]
-    rapdict = []
-    rhyme_list_generator(lyrics, rapdict, all_possible_rhymes)
-    print rapdict
+    if training == 1:
+        # rapdict is just the list containing smaller lists as follows;
+        # [the text of the line, the number of syllables in the line, the number of the rhyme scheme of the line]
 
-    # makes a dataset
-    ds = SupervisedDataSet(4,4)
-    # the dataset is in the form of the amount of syllables and rhyme scheme of TWO lines that are next to each other in the song.
+        rapdict = []
+        rhyme_list_generator(lyrics, rapdict, all_possible_rhymes)
+        print "\n\nAlright, here's the information it will be working with - in the form of lyric, syllables, and rhyming scheme"
+        print rapdict
+
+        # makes a dataset
+        ds = SupervisedDataSet(4,4)
+        # the dataset is in the form of the amount of syllables and rhyme scheme of TWO lines that are next to each other in the song.
 
 
-    for i in rapdict[:-3]:
-        if i != "" and rapdict[rapdict.index(i) + 1] != "" and rapdict[rapdict.index(i) + 2] != "" and rapdict[rapdict.index(i) + 3] != "":
-            # twobars is just a list containing the aspects of two lines in a row
-            twobars = [i[1], i[2], rapdict[rapdict.index(i) + 1][1], rapdict[rapdict.index(i) + 1][2], rapdict[rapdict.index(i) + 2][1], rapdict[rapdict.index(i) + 2][2], rapdict[rapdict.index(i) + 3][1], rapdict[rapdict.index(i) + 3][2]]
+        for i in rapdict[:-3]:
+            if i != "" and rapdict[rapdict.index(i) + 1] != "" and rapdict[rapdict.index(i) + 2] != "" and rapdict[rapdict.index(i) + 3] != "":
+                # twobars is just a list containing the aspects of two lines in a row
+                twobars = [i[1], i[2], rapdict[rapdict.index(i) + 1][1], rapdict[rapdict.index(i) + 1][2], rapdict[rapdict.index(i) + 2][1], rapdict[rapdict.index(i) + 2][2], rapdict[rapdict.index(i) + 3][1], rapdict[rapdict.index(i) + 3][2]]
 
-            # twobars gets formatted into floating point values between 0 and 1 so it can be entered into the dataset
-            ds.addSample((twobars[0] / float(20), int(twobars[1]) / float(len(all_possible_rhymes)), twobars[2] / float(20), int(twobars[3]) / float(len(all_possible_rhymes))), (twobars[4] / float(20), int(twobars[5]) / float(len(all_possible_rhymes)), twobars[6] / float(20), int(twobars[7]) / float(len(all_possible_rhymes))))
+                # twobars gets formatted into floating point values between 0 and 1 so it can be entered into the dataset
+                ds.addSample((twobars[0] / float(20), int(twobars[1]) / float(len(all_possible_rhymes)), twobars[2] / float(20), int(twobars[3]) / float(len(all_possible_rhymes))), (twobars[4] / float(20), int(twobars[5]) / float(len(all_possible_rhymes)), twobars[6] / float(20), int(twobars[7]) / float(len(all_possible_rhymes))))
 
-    # printing the dataset
-    print "\n\nAlright, here is the dataset."
-    print ds
+        # printing the dataset
+        print "\n\nAlright, here is the dataset."
+        print ds
 
-    # Only uncomment this if you are training it on lyrics yourself.
-    # this part gets a neural network, trains it on lyrics and syllables and then saves it.
-    """
-    net = buildNetwork(4,6,6,6,4,recurrent=True)
-    t = BackpropTrainer(net,learningrate=0.05,momentum=0.5,verbose=True)
-    t.trainOnDataset(ds,100)
-    t.testOnData(verbose=True)
-
-    fileObject = open('trained_net', 'w')
-    pickle.dump(net, fileObject)
-    fileObject.close()
-    """
-
-    # This loads a neural network that has already been trained on an actual rap song - so it knows how the rhymes and syllables should fit together
-    fileObject = open('trained_net_420000','r')
-    net = pickle.load(fileObject)
-    t = BackpropTrainer(net,learningrate=0.01,momentum=0.5,verbose=True)
-
-    # uncomment this line too if you're training.
-    #t.trainOnDataset(ds, 200)
 
     #just to make sure it doesn't keep using the same lyric over and over
     lyricsused = []
-    # takes a list of 2 values, syllables and rhyming scheme
 
-    trainingcount = 1000
+    trainingcount = 0
 
     # The part that actually writes a rap.
     final_song = open("neural_rap.txt", "r+")
@@ -189,26 +216,26 @@ def main():
     # The number 3 at the end of this line can be tweaked- it's just so things don't get too repetitive/drawn out.
     # for example; if i had 30 lines to draw from, I wouldn't want to try and rearrange them into a song with 30 lines.
     # it would be much better if i only tried to take 10 rhyming lines and make a song with those.
-    while len(song) < len(lyrics) / 3:
-        # uncomment this next line if you're training the network.
-        #t.trainOnDataset(ds, 100)
-        verse = writearap([(random.choice(range(1,20))) / 20.0 , (random.choice(range(1, len(all_possible_rhymes)))) / float(len(all_possible_rhymes)), (random.choice(range(1, 20))) / 20.0, (random.choice(range(1, len(all_possible_rhymes)))) / float(len(all_possible_rhymes))], net, rapdict, all_possible_rhymes, lyricsused, song)
-        if len(verse) > 2:
-            for line in lyricsused:
-                final_song.write(str(line) + "\n")
+    if training == 0:
+        while len(song) < len(lyrics) / 3 and len(song) < 50:
+            verse = writearap([(random.choice(range(1,20))) / 20.0 , (random.choice(range(1, len(all_possible_rhymes)))) / float(len(all_possible_rhymes)), (random.choice(range(1, 20))) / 20.0, (random.choice(range(1, len(all_possible_rhymes)))) / float(len(all_possible_rhymes))], net, rapdict, all_possible_rhymes, lyricsused, song)
+            if len(verse) > 3: # this number can be adjusted - usually the short verses it generates are low quality.
+                for line in lyricsused:
+                    final_song.write(str(line) + "\n")
 
-                # actually write the line to the song
-                song.append(line)
-            final_song.write("\n")
-            print "Just wrote a verse to the file... - " + str(lyricsused)
-            lyricsused = []
-        #uncomment these if you wish to train the network, you'll also have to uncomment above lines that actually train the network (around line 145)
-        """
-        fileObject = open('trained_net_' + str(trainingcount), 'w')
-        pickle.dump(net, fileObject)
-        fileObject.close()
-        trainingcount += 1000
-        """
+                    # actually write the line to the song
+                    song.append(line)
+                final_song.write("\n")
+                print "Just wrote a verse to the file... - " + str(lyricsused)
+                lyricsused = []
+
+    if training == 1:
+        while True:
+            epochs_per_iteration = 100
+            trainingcount += epochs_per_iteration
+            t.trainOnDataset(ds, epochs_per_iteration)
+            print "just wrote " + str(trainingcount) + "/" + "..."
+            savenetwork(net, all_possible_rhymes, trainingcount)
 
 main()
 print "\nDone! Check neural_rap.txt for the finished rap."
