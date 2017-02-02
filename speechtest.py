fefb88c1e82e1a8be09f9539b8283f3dd1b3e5ec
Robbie Barrat <robbiebarrat@gmail.com>
2017-02-01 23:32:59 -0500

    Ease of use and increased functionality
    >Ease of use
    variables at the top allow for easier tweaking for more inexperienced users
    >increased functionality
    I ran into a problem when i tried to have it rap ~300 lines... this fixes that so it should be able to handle a LOT more lines at once - way more than it will ever need to.

--- speechtest.py
+++ speechtest.py
@@ -1,37 +1,48 @@
 import pyttsx
 import subprocess
 import threading
-import string
 from time import sleep
 from threading import Thread
+
+# VARIABLES
+beat = "beat.mp3" 
+slowdown_rate = 30 #higher value means slower speech... keep it between ~50 and 0 depending on how fast the beat is.
+intro = 14 #amount in seconds it takes from the start of the beat to when the rapping should begin... how many seconds the AI waits to start rapping.
+
 
 def play_mp3(path):
     subprocess.Popen(['mpg123', '-q', path]).wait()
 
 
 engine = pyttsx.init()
+
+def letters(input):
+    valids = []
+    for character in input:
+        if character.isalpha() or character == "," or character == "'" or character == " ":
+            valids.append(character)
+    return ''.join(valids)
+
 lyrics = open("neural_rap.txt").read().split("\n") #this reads lines from a file called 'neural_rap.txt'
+#print lyrics
 rate = engine.getProperty('rate')
-engine.setProperty('rate', rate - 30)
+engine.setProperty('rate', rate - slowdown_rate)
 voices = engine.getProperty('voices')
 
 wholesong = ""
 for i in lyrics:
-    wholesong += i
-    wholesong += " ... ... "
-
-printable = set(string.printable)
+    wholesong += i + " ... "
 
 
 def sing():
-    for i in lyrics:
-        engine.say(str(filter(lambda x: x in printable, wholesong)))
-        engine.runAndWait()
+    for line in wholesong.split(" ... "):
+        engine.say(str(line))
+    engine.runAndWait()
 
 def beat():
     play_mp3("beat.mp3")
 
 
 Thread(target=beat).start()
-sleep(14) # just waits a little bit to start talking
+sleep(intro) # just waits a little bit to start talking
 Thread(target=sing).start()
